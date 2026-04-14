3be4fd76891b9194d49a34cfd681a5e9899201ec
CHABDULQUDOOS682 <abdulqudoos682@gmail.com>
2026-04-15 01:09:03 +0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-User.find_or_initialize_by(email: "super_admin@example.com").tap do |user|
-  user.password = "Password@123"
-  user.password_confirmation = "Password@123"
-  user.role = "super_admin"
-  user.save!
-end
 User.find_or_initialize_by(email: "admin@example.com").tap do |user|
   user.password = "Password@123"
   user.password_confirmation = "Password@123"
