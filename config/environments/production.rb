3e15011479fea92171a9a083dd991e6d220e981a
CHABDULQUDOOS682 <abdulqudoos682@gmail.com>
2026-06-02 22:13:05 +0500

    Add APP_HOST and APP_PROTOCOL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("APP_HOST"),
+    host: ENV.fetch("APP_HOST", "localhost"),
     protocol: ENV.fetch("APP_PROTOCOL", "https")
   }
 
